4f6c8e0efc4d41ec6adf6b1f798b9f2853f13343
Sergey Chebotar <s.chebotar@gmail.com>
2020-02-12 20:42:46 +0300

    Решение без отсечения

--- PathFinderTask.cs
+++ PathFinderTask.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 
 namespace RoutePlanning
 {
@@ -8,16 +9,38 @@ namespace RoutePlanning
 	{
 		public static int[] FindBestCheckpointsOrder(Point[] checkpoints)
 		{
-			var bestOrder = MakeTrivialPermutation(checkpoints.Length);
-			return bestOrder;
+			var permutation = new int[checkpoints.Length];
+			permutation[0] = 0;
+			var permutationsList = new List<int[]>();
+			MakePermutations(permutation, 1, permutationsList);
+
+			var pathLengths = new double[permutationsList.Count];
+			for (int i = 0; i < permutationsList.Count; i++)
+			{
+				pathLengths[i] = PointExtensions.GetPathLength(checkpoints, permutationsList[i]);
+			}
+
+			return permutationsList[Array.IndexOf(pathLengths, pathLengths.Min())];
 		}
 
-		private static int[] MakeTrivialPermutation(int size)
+		static void MakePermutations(int[] permutation, int position, List<int[]> permutationsList)
 		{
-			var bestOrder = new int[size];
-			for (int i = 0; i < bestOrder.Length; i++)
-				bestOrder[i] = i;
-			return bestOrder;
+			if (position == permutation.Length)
+			{
+				permutationsList.Add(permutation.ToArray());
+				return;
+			}
+			else
+			{
+				for (int i = 0; i < permutation.Length; i++)
+				{
+					var index = Array.IndexOf(permutation, i, 0, position);
+					if (index != -1)
+						continue;
+					permutation[position] = i;
+					MakePermutations(permutation, position + 1, permutationsList);
+				}
+			}
 		}
 	}
 }
